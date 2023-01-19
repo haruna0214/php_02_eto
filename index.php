c1f3f064b35740773268b78373c5f92d1b793bdd
“Haruna <ex436000@gmail.com>
2023-01-19 17:22:55 +0900

    Webアプリ 干支計算機

--- index.php
+++ index.php
@@ -7,5 +7,18 @@ function eto( $year ) {
     return $etos[($year - 4) % 12];
 }
 $year = filter_input(INPUT_GET, "year", FILTER_VALIDATE_INT);
-
 ?>
+<!-- プログラムの読みやすさを重視して、headタグとbodyタグを省略 -->
+<!DOCTYPE html>
+    <title>干支計算機</title>
+    <h1>干支計算機</h1>
+<?php if (empty($year)): ?>
+<p>数字を入力してください</p>
+<form method="get">
+    <label>年</label>:
+    <!-- value属性は、初期状態で現在の西暦を表示するように指定 -->
+    <input name="year" type="number" value="<?= h(date( "Y" )) ?>">
+</form>
+<?php else: ?>
+<p><?= h($year) ?>年は<?= eto($year) ?>年です。</p>
+<?php endif; ?>
